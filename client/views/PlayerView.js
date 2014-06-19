1990fece73b7ba7474fd22fb0409781e16998629
hisimagination <joel.design@icloud.com>
2014-06-19 10:23:03 -0700

    added styles

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -3,7 +3,7 @@ var PlayerView = Backbone.View.extend({
 
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
-  el: '<div><p></p><audio controls autoplay></div>',
+  el: '<div class="col-md-8"><blockquote id="player"><p><i class="fa fa-music fa-2x"></i> <strong id="current"></strong></p><audio controls autoplay style="width:100%;"></blockquote></div>',
 
   initialize: function() {
   },
@@ -23,7 +23,7 @@ var PlayerView = Backbone.View.extend({
     return this.$el                       // <DIV><p></p><audio></div>
       .find('audio')                      // <div><p></p><AUDIO></div>
         .attr('src', this.model ? this.model.get('url') : '')
-      .parent().find('p')                 // <div><P></p><audio></div>
+      .parent().find('#current')                 // <div><P></p><audio></div>
         .text(this.model.get('title'));   // <div><p>****</p><audio></div>
   }
 
